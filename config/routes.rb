9156aab67ed2af0d59804692c9a010e0164e8fab
krmclellan <kmclella@KarenMclansiMac.hitronhub.home>
2017-12-30 13:27:35 -0700

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+
+resources :articles
+
+root 'welcome#index'
+get '/' => "welcome#index"
+get "/projects" => "welcome#projects"
+  get "/about" => "welcome#about"
+  get "/contact" => "welcome#contact"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
